1fdc633dcf78440199f75aa43cbac53ef79cefb6
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-06 11:37:46 +0000

    fixed useAccounts to correctly fetch data

--- src/useAccounts.js
+++ src/useAccounts.js
@@ -1,18 +1,17 @@
 import React, {useEffect, useState} from "react";
 
 
-const useAccounts = () => {
+function useAccounts() {
     const [accounts, setAccounts] = useState([])
 
     useEffect(()=>{
         fetch('http://localhost:8000/accounts')
-            .then((data)=>{return data.json()})
+            .then((data)=>data.json())
             .then((data)=>{
-
                 setAccounts(data)
             })
-    })
-return accounts
+    }, [])
+    return accounts
 
 }
 
