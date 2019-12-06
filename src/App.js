72f631734393c76b5352d46c83a54411c9c96289
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-06 15:12:28 +0000

    added AccountsAbove

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import './App.css'
 import Header from './Header'
 import DisplayAccounts from './displayAccounts'
+import DisplayAccountsAbove from './displayAccountsAbove'
 
 function App() {
   return (
     <div>
       <Header />
       <DisplayAccounts />
+      <DisplayAccountsAbove />
     </div>
   )
 }
