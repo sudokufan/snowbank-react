fc65c9a746dd65970b3374212e679344d1f9637a
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-06 15:18:57 +0000

    reverted to previous working version, branched off

--- src/App.js
+++ src/App.js
@@ -2,14 +2,13 @@ import React from 'react';
 import './App.css'
 import Header from './Header'
 import DisplayAccounts from './displayAccounts'
-import DisplayAccountsAbove from './displayAccountsAbove'
 import Routes from "./routes";
 
 function App() {
   return (
     <div>
       <Header />
-      <Routes />
+      <DisplayAccounts />
     </div>
   )
 }
