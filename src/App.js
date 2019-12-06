a5073d85b15683fbd8ae16a915c4607f554e00f5
sudokufan <alexrichardgeorge@protonmail.com>
2019-12-06 13:34:14 +0000

    imported Header to App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Header from './Header'
 import DisplayAccounts from './displayAccounts'
 
 function App() {
   return (
     <div>
+      <Header />
       <DisplayAccounts />
     </div>
   )
